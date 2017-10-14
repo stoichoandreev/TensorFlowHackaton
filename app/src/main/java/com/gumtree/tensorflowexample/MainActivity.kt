1bc414d290131c7053234cad674a4140f5214b87
stoichoandreev <stoichoandreev@yahoo.com>
2017-10-14 16:00:58 +0100

    Initial commit

---
+++ app/src/main/java/com/gumtree/tensorflowexample/MainActivity.kt
@@ -0,0 +1,155 @@
+package com.gumtree.tensorflowexample
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import com.flurgle.camerakit.CameraListener
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+
+class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private val MIN_PRICE = 50
+        private val MAX_PRICE = 300
+        private val INPUT_SIZE = 224
+        private val IMAGE_MEAN = 117
+        private val IMAGE_STD = 1f
+        private val INPUT_NAME = "input"
+        private val OUTPUT_NAME = "output"
+        private val MODEL_FILE = "file:///android_asset/tensorflow_inception_graph.pb"
+        private val LABEL_FILE = "file:///android_asset/imagenet_comp_graph_label_strings.txt"
+    }
+
+    private var classifier: Classifier? = null
+    private val executor: ExecutorService by lazy { Executors.newSingleThreadExecutor() }
+    private lateinit var picture: ByteArray
+    private val rand: Random by lazy(LazyThreadSafetyMode.NONE) { Random() }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        camera_view!!.setCameraListener(object : CameraListener() {
+            override fun onPictureTaken(picture: ByteArray) {
+                super.onPictureTaken(picture)
+
+                this@MainActivity.picture = picture
+
+                var bitmap = BitmapFactory.decodeByteArray(picture, 0, picture.size)
+                bitmap = Bitmap.createScaledBitmap(bitmap, INPUT_SIZE, INPUT_SIZE, false)
+                image_view_result!!.setImageBitmap(bitmap)
+                val results = classifier!!.recognizeImage(bitmap)
+                displayImageRecognitionResult(results)
+            }
+        })
+
+//        button_toggle_camera!!.setOnClickListener { camera_view!!.toggleFacing() }
+        button_detect_object!!.setOnClickListener { camera_view!!.captureImage() }
+
+        initTensorFlowAndLoadModel()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        camera_view!!.start()
+    }
+
+    override fun onPause() {
+        camera_view!!.stop()
+        super.onPause()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        executor.execute({ classifier!!.close() })
+    }
+
+    private fun initTensorFlowAndLoadModel() {
+        executor.execute({
+            try {
+                classifier = TensorFlowImageClassifier.create(
+                        assets,
+                        MODEL_FILE,
+                        LABEL_FILE,
+                        INPUT_SIZE,
+                        IMAGE_MEAN,
+                        IMAGE_STD,
+                        INPUT_NAME,
+                        OUTPUT_NAME)
+                makeButtonVisible()
+            } catch (e: Exception) {
+                throw RuntimeException("Error initializing TensorFlow!", e)
+            }
+        })
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        finish()
+    }
+
+    private fun makeButtonVisible() {
+        runOnUiThread { button_detect_object!!.visibility = View.VISIBLE }
+    }
+
+    private fun displayImageRecognitionResult(results: List<Classifier.Recognition>) {
+        results_information_container.visibility = View.VISIBLE
+        results_container.removeAllViews()
+
+        val inflater = getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        var counter = 1
+        if (results.isNotEmpty()) {
+            for (recognizedImage in results) {
+                val childView = inflater.inflate(R.layout.view_image_recognition_result, results_container, false)
+
+                val resultTextView = childView.findViewById(R.id.text_view_result) as TextView
+                val gtSearchButton = childView.findViewById(R.id.gt_search) as ImageView
+                val ecgSearchButton = childView.findViewById(R.id.ecg_search) as ImageView
+
+                gtSearchButton.setOnClickListener({ callGTSearch(recognizedImage) })
+                ecgSearchButton.setOnClickListener({ callECGSearch() })
+
+                val text = recognizedImage.title ?: ""
+                resultTextView.text = "$counter. $text"
+                results_container.addView(childView)
+                counter++
+            }
+        } else {
+            val noResultsView = inflater.inflate(R.layout.view_empty_image_recognition_result, results_container, false)
+            results_container.addView(noResultsView)
+        }
+    }
+
+    private fun callGTSearch(recognizedImage: Classifier.Recognition) {
+        val resultIntent = Intent()
+
+        val value = rand.nextInt((MAX_PRICE - MIN_PRICE) + 1) + MIN_PRICE
+
+        resultIntent.putExtra("imageText", recognizedImage.title)
+        resultIntent.putExtra("imageSuggestedPrice", value.toString())
+//        resultIntent.putExtra("imageThumb", picture)
+        setResult(Activity.RESULT_OK, resultIntent)
+        finish()
+    }
+
+    private fun callECGSearch() {
+        val ebayIntent = packageManager.getLaunchIntentForPackage("com.ebay.mobile")
+        if (ebayIntent != null) {
+            startActivity(ebayIntent)
+        } else {
+            Toast.makeText(this, "Can not find Ebay mobile application on this device", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
---
+++ app/src/main/java/com/gumtree/tensorflowexample/TensorFlowImageClassifier.kt
@@ -0,0 +1,166 @@
+package com.gumtree.tensorflowexample
+
+import android.annotation.TargetApi
+import android.content.res.AssetManager
+import android.graphics.Bitmap
+import android.os.Trace
+import android.support.annotation.NonNull
+import android.util.Log
+import org.tensorflow.contrib.android.TensorFlowInferenceInterface
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStreamReader
+import java.util.*
+
+class TensorFlowImageClassifier : Classifier {
+
+    private var inputName: String? = null
+    private var outputName: String? = null
+    private var inputSize: Int = 0
+    private var imageMean: Int = 0
+    private var imageStd: Float = 0.0f
+
+    //Buffers.
+    private val labels: Vector<String> by lazy { Vector<String>() }
+    private var intValues: IntArray? = null
+    private var floatValues: FloatArray? = null
+    private var outputs: FloatArray? = null
+    private var outputNames: Array<String>? = null
+
+    private var inferenceInterface: TensorFlowInferenceInterface? = null
+
+    /**
+     * Initializes a native TensorFlow session for classifying images.
+     *
+     * @param assetManager  The asset manager to be used to load assets.
+     * @param modelFilename The filepath of the model GraphDef protocol buffer.
+     * @param labelFilename The filepath of label file for classes.
+     * @param inputSize     The input size. A square image of inputSize x inputSize is assumed.
+     * @param imageMean     The assumed mean of the image values.
+     * @param imageStd      The assumed std of the image values.
+     * @param inputName     The label of the image input node.
+     * @param outputName    The label of the output node.
+     * @throws IOException
+     */
+    companion object {
+        private val TAG = "GTImageClassifier"
+        // Only return this many results with at least this confidence.
+        private val MAX_RESULTS = 3
+        private val THRESHOLD = 0.1f
+
+        @Throws(IOException::class)
+        fun create(
+                @NonNull assetManager: AssetManager,
+                @NonNull modelFilename: String,
+                @NonNull labelFilename: String,
+                inputSize: Int,
+                imageMean: Int,
+                imageStd: Float,
+                inputName: String,
+                outputName: String): Classifier {
+            val tensorFlowImageClassifier = TensorFlowImageClassifier()
+            tensorFlowImageClassifier.inputName = inputName
+            tensorFlowImageClassifier.outputName = outputName
+
+            // Read the label names into memory.
+            // TODO this reading can be optimized.
+            val actualFilename = labelFilename.split("file:///android_asset/".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()[1]
+            Log.i(TAG, "Reading labels: " + actualFilename)
+            val br: BufferedReader? = BufferedReader(InputStreamReader(assetManager.open(actualFilename)))
+            br?.readLines()?.forEach {
+                tensorFlowImageClassifier.labels.add(it)
+            }
+            br?.close()
+
+            tensorFlowImageClassifier.inferenceInterface = TensorFlowInferenceInterface()
+            if (tensorFlowImageClassifier.inferenceInterface!!.initializeTensorFlow(assetManager, modelFilename) != 0) {
+                throw RuntimeException("TF initialization failed")
+            }
+            // The shape of the output is [N, NUM_CLASSES], where N is the batch size.
+            val numClasses = tensorFlowImageClassifier.inferenceInterface!!.graph().operation(outputName).output(0).shape().size(1).toInt()
+            Log.i(TAG, "Read " + tensorFlowImageClassifier.labels.size + " labels, output layer size is " + numClasses)
+
+            // Ideally, inputSize could have been retrieved from the shape of the input operation.  Alas,
+            // the placeholder node for input in the graphdef typically used does not specify a shape, so it
+            // must be passed in as a parameter.
+            tensorFlowImageClassifier.inputSize = inputSize
+            tensorFlowImageClassifier.imageMean = imageMean
+            tensorFlowImageClassifier.imageStd = imageStd
+
+            // Pre-allocate buffers.
+            tensorFlowImageClassifier.outputNames = arrayOf(outputName)
+            tensorFlowImageClassifier.intValues = IntArray(inputSize * inputSize)
+            tensorFlowImageClassifier.floatValues = FloatArray(inputSize * inputSize * 3)
+            tensorFlowImageClassifier.outputs = FloatArray(numClasses)
+
+            return tensorFlowImageClassifier
+        }
+    }
+
+    @TargetApi(18)
+    override fun recognizeImage(bitmap: Bitmap): List<Classifier.Recognition> {
+        // Log this method so that it can be analyzed with systrace.
+        Trace.beginSection("recognizeImage")
+
+        Trace.beginSection("preprocessBitmap")
+        // Preprocess the image data from 0-255 int to normalized float based
+        // on the provided parameters.
+        bitmap.getPixels(intValues, 0, bitmap.width, 0, 0, bitmap.width, bitmap.height)
+        for (i in intValues!!.indices) {
+            val value = intValues!![i]
+            floatValues?.set(i * 3 + 0, ((value shr 16 and 0xFF) - imageMean) / imageStd)
+            floatValues?.set(i * 3 + 1, ((value shr 8 and 0xFF) - imageMean) / imageStd)
+            floatValues?.set(i * 3 + 2, ((value and 0xFF) - imageMean) / imageStd)
+        }
+        Trace.endSection()
+
+        // Copy the input data into TensorFlow.
+        Trace.beginSection("fillNodeFloat")
+        inferenceInterface!!.fillNodeFloat(
+                inputName!!, intArrayOf(1, inputSize, inputSize, 3), floatValues)
+        Trace.endSection()
+
+        // Run the inference call.
+        Trace.beginSection("runInference")
+        inferenceInterface!!.runInference(outputNames!!)
+        Trace.endSection()
+
+        // Copy the output Tensor back into the output array.
+        Trace.beginSection("readNodeFloat")
+        inferenceInterface!!.readNodeFloat(outputName, outputs)
+        Trace.endSection()
+
+        // Find the best classifications.
+        val priorityQueue = PriorityQueue(3,
+                Comparator<Classifier.Recognition> { lhs, rhs ->
+                    //                     Intentionally reversed to put high confidence at the head of the queue.
+                    java.lang.Float.compare(rhs.confidence!!, lhs.confidence!!)
+                })
+
+        outputs!!.indices
+                .filter { outputs!![it] > THRESHOLD }
+                .mapTo(priorityQueue) {
+                    Classifier.Recognition(
+                            "" + it, if (labels.size > it) labels[it] else "unknown", outputs!![it], null)
+                }
+
+        val recognitions = ArrayList<Classifier.Recognition>()
+        val recognitionsSize = Math.min(priorityQueue.size, MAX_RESULTS)
+        for (i in 0 until recognitionsSize) {
+            recognitions.add(priorityQueue.poll())
+        }
+        Trace.endSection() //"recognizeImage"
+        return recognitions
+    }
+
+    override fun enableStatLogging(debug: Boolean) {
+        inferenceInterface?.enableStatLogging(debug)
+    }
+
+    override fun getStatString(): String? =
+            inferenceInterface?.statString
+
+    override fun close() {
+        inferenceInterface!!.close()
+    }
+}
